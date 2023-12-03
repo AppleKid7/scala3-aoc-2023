d3483ce605fc43d45a2bc804aa0eeb46f430329f
Ibrahim Moreno <kvltd00d@gmail.com>
2023-12-03 11:06:50 -0500

    day2 part 1 progress parsing games

--- src/main/scala/main/Day2.scala
+++ src/main/scala/main/Day2.scala
@@ -3,20 +3,45 @@ package main
 import java.io.IOException
 import zio.*
 import zio.stream.*
+import scala.annotation.tailrec
 
 object Day2 extends ZIOAppDefault {
-
-  enum Balls {
-    case red, green, blue
-  }
-
   final case class Game(id: Int, draws: Set[Map[String, Int]])
 
   val stream =
     ZStream.fromFileName("day2Input.txt").via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
 
-//   @tailrec
-  private def parseDraw(input: String): Set[Map[String, Int]] = ???
+  private def parseGame(input: String): Set[Map[String, Int]] = {
+    @tailrec
+    def loop(input: List[String], acc: Set[Map[String, Int]]): Set[Map[String, Int]] = {
+      input match {
+        case Nil => acc
+        case head :: tail => loop(tail, acc + parseDraw(head))
+      }
+    }
+    loop(input.split(";").toList, Set.empty[Map[String, Int]])
+  }
+
+  private def addToMap(key: String, value: Int, map: Map[String, Int]): Map[String, Int] =
+    map.updatedWith(key) {
+      case None => Some(value)
+      case Some(oldValue) => Some(oldValue + value)
+    }
+
+  private def parseDraw(input: String): Map[String, Int] = {
+    @tailrec
+    def loop(current: List[String], acc: Map[String, Int]): Map[String, Int] = {
+      if (current.isEmpty) acc
+      else
+        current match {
+          case s"$num blue" :: tail => loop(tail, addToMap("blue", num.toInt, acc))
+          case s"$num red" :: tail => loop(tail, addToMap("red", num.toInt, acc))
+          case s"$num green" :: tail => loop(tail, addToMap("green", num.toInt, acc))
+          case _ => acc
+        }
+    }
+    loop(input.split(",").toList.map(_.strip()), Map.empty[String, Int])
+  }
 
   val pipeLine: ZPipeline[Any, RuntimeException, String, (Int, Set[Map[String, Int]])] =
     ZPipeline.mapZIO { line =>
@@ -27,13 +52,13 @@ object Day2 extends ZIOAppDefault {
           case _ => ZIO.fail(new RuntimeException("malformed input file!"))
         }
         drawStrings = separated(1).split(";")
-        sets = drawStrings.flatMap(setString => parseDraw(setString)).toSet
+        sets = drawStrings.flatMap(setString => parseGame(setString)).toSet
       } yield (gameId -> sets)
     }
 
   val sink =
     ZSink.collectAll[(Int, Set[Map[String, Int]])].mapZIO { lines =>
-      ZIO.foreach(lines)(line => Console.printLine(s"id: ${line._1}, first: ${line._2.headOption}"))
+      ZIO.foreach(lines)(line => Console.printLine(s"id: ${line._1}, first: ${line._2}"))
     }
 
   val run = stream.via(pipeLine).run(sink)
